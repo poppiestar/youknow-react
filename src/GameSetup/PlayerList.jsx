60fdf74a6fbd6e76eb8d28128c45fcb5347c3501
Drew Miller <poppiestar@users.noreply.github.com>
2017-07-11 12:37:03 +0100

    Fix warning on binding to wrong component

--- src/GameSetup/PlayerList.jsx
+++ src/GameSetup/PlayerList.jsx
@@ -2,10 +2,14 @@
 import React, { Component } from 'react';
 
 export default class PlayerList extends Component {
+    removePlayer (id) {
+        this.props.removePlayer(id);
+    }
+
     render () {
         const players = this.props.players.map((player, i) => {
             return (
-                <li key={i}>{player.name} <button onClick={this.props.removePlayer.bind(this, player)}>remove</button></li>
+                <li key={i}>{player.name} <button onClick={this.removePlayer.bind(this, player)}>remove</button></li>
             );
         });
 
