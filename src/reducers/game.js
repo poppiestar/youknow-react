b1c221a6fc19498950f27ccb0f99f8e893c21f09
Drew Miller <poppiestar@users.noreply.github.com>
2017-07-11 12:37:13 +0100

    Create reducers for enter score page

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -1,15 +1,21 @@
 
-import { SET_STAGE, SET_WINNER, Stage } from '../actions';
+import { SET_STAGE, SET_WINNER, ADD_SCORE, SUBTRACT_SCORE, Stage } from '../actions';
 
-const game = (state = { stage: Stage.GAME_SETUP } , action) => {
+const game = (state = { stage: Stage.GAME_SETUP, roundScore: 0 } , action) => {
+    let newState;
+
     switch (action.type) {
         case SET_STAGE:
-            return {
-                stage: action.value
-            };
+            return Object.assign({}, state, { stage: action.value });
 
         case SET_WINNER:
             return Object.assign({}, state, { roundWinner: action.value });
+
+        case ADD_SCORE:
+            return Object.assign({}, state, { roundScore: state.roundScore + action.value });
+
+        case SUBTRACT_SCORE:
+            return Object.assign({}, state, { roundScore: state.roundScore - action.value });;
 
         default:
             return state;
