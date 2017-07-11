18e2d151482db6def9ee183c7d72da601d996fd7
Drew Miller <poppiestar@users.noreply.github.com>
2017-07-11 12:37:13 +0100

    Wire enter score actions into component

--- src/VisibleEnterScore.js
+++ src/VisibleEnterScore.js
@@ -1,17 +1,26 @@
 
 import { connect } from 'react-redux';
 
+import { nextRound, addScore, subtractScore } from './actions';
 import EnterScore from './EnterScore';
 
 const mapStateToProps = (state) => {
     return {
-
+        winner: state.players[state.game.winner]
     };
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
-
+        addScore: (value) => {
+            dispatch(addScore(value));
+        },
+        subtractScore: (value) => {
+            dispatch(subtractScore(value));
+        },
+        nextRound: () => {
+            dispatch(nextRound());
+        }
     };
 };
 
