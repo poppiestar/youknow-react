4e1e5950bd0c55bc01d54b1436e5ec8c48e87ab1
Drew Miller <poppiestar@users.noreply.github.com>
2017-07-11 12:37:07 +0100

    Convert to stateless function

--- src/GameSetup/GoalSelect.jsx
+++ src/GameSetup/GoalSelect.jsx
@@ -1,22 +1,14 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 
 const goals = [300, 500, 750, 1000];
 
-export default class GoalSelect extends Component {
-    handleChange (e) {
-        this.props.updateGoal(e.currentTarget.value);
-    }
-
-    render () {
-        const goalItems = goals.map((item, i) => {
+const GoalSelect = ({ goal, updateGoal }) => (
+    <select value={goal} onChange={(e) => updateGoal(e.currentTarget.value)}>
+        {goals.map((item, i) => {
             return <option key={i} value={item}>{item}</option>;
-        });
+        })}
+    </select>
+);
 
-        return (
-            <select value={this.props.goal} onChange={this.handleChange.bind(this)}>
-                {goalItems}
-            </select>
-        );
-    }
-}
+export default GoalSelect;
