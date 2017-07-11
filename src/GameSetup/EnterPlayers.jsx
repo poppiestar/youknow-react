90ef32be9c4721f9c6026db8db54ab2200e447dd
Drew Miller <poppiestar@users.noreply.github.com>
2017-07-11 12:37:08 +0100

    Convert to stateless function

--- src/GameSetup/EnterPlayers.jsx
+++ src/GameSetup/EnterPlayers.jsx
@@ -1,17 +1,15 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 
 import VisiblePlayerList from './VisiblePlayerList';
 import VisibleNewPlayer from './VisibleNewPlayer';
 
-export default class EnterPlayers extends Component {
-    render () {
-        return (
-            <div>
-                <h2>Enter Players</h2>
-                <VisiblePlayerList />
-                <VisibleNewPlayer />
-            </div>
-        );
-    }
-}
+const EnterPlayers = () => (
+    <div>
+        <h2>Enter Players</h2>
+        <VisiblePlayerList />
+        <VisibleNewPlayer />
+    </div>
+);
+
+export default EnterPlayers;
