a41f5aa3b3f5b95aee889c383e0858bdea24a65b
Drew Miller <poppiestar@users.noreply.github.com>
2017-07-23 10:48:36 +0100

    Improved ErrorMessage

--- src/ErrorMessage.jsx
+++ src/ErrorMessage.jsx
@@ -1,6 +1,12 @@
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ErrorMessage = ({ message }) => {
-    return message ? <p className="error">{this.props.message}</p> : null;
-}
+const ErrorMessage = ({ message }) =>
+  message && <p className="error">{message}</p>;
+
+ErrorMessage.propTypes = {
+  message: PropTypes.string.isRequired
+};
+
+export default ErrorMessage;
