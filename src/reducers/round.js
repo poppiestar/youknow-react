0272ff1fbe98e79f27d1a5e2c309588d21922580
Drew Miller <poppiestar@users.noreply.github.com>
2017-07-29 10:11:01 +0100

    Set round values when selecting a round winner

--- src/reducers/round.js
+++ src/reducers/round.js
@@ -10,7 +10,7 @@ const initialState = {
 const game = (state: RoundState = initialState, action: Action): RoundState => {
     switch (action.type) {
         case 'ROUND:SET_WINNER':
-            return Object.assign({}, state, { winner: action.winner });
+            return Object.assign({}, initialState, { winner: action.winner });
 
         case 'ROUND:ADD_SCORE':
             return Object.assign({}, state, { score: state.score + action.value });
